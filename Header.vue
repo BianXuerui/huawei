22cc2b04e5b7bb7a28c0f0f26f2937cdcecd3261
BianXuerui <50104688+BianXuerui@users.noreply.github.com>
2019-05-09 18:53:21 +0800

    Header.vue
    面包屑导航由组件更改为源码手写,添加了css注释

--- Header.vue
+++ Header.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="hw-header">
-    <div>
-      <el-breadcrumb separator="|">
-        <el-breadcrumb-item :to="{ path: '/' }"><a href="">首页</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">华为官网</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">荣耀俱乐部</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">花粉俱乐部</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">V码(优购码)</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">企业码</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">Select Region</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">更多精彩</a></el-breadcrumb-item>
-        <div class="gdjc">
-          <ul>
-            <li><a href="">EMUI</a></li>
-            <li><a href="">应用市场</a></li>
-            <li><a href="">华为终端云空间</a></li>
-            <li><a href="">开发者联盟</a></li>
-          </ul>
-        </div>
-      </el-breadcrumb>
+    <div class="layout">
+    <div class="s-sub">
+      <ul>
+        <li><a href="">首页</a></li>
+        <li><a href="">华为官网</a></li>
+        <li><a href="">荣耀俱乐部</a></li>
+        <li><a href="">花粉俱乐部</a></li>
+        <li><a href="">V码(优购码)</a></li>
+        <li><a href="">企业购</a></li>
+        <li><a href="">Select Regin</a></li>
+        <li><a href="">更多精彩</a></li>
+      </ul>
     </div>
-    <div>
-      <el-breadcrumb separator="|">
-        <el-breadcrumb-item :to="{ path: '/' }"><a href="">请登录</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">注册</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">我的订单</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">客户服务</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">网站导航</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">手机版</a></el-breadcrumb-item>
-        <el-breadcrumb-item><a href="">购物车</a></el-breadcrumb-item>
-      </el-breadcrumb>
+    <div class="s-main">
+        <ul>
+        <li><a href="">请登录</a></li>
+        <li><a href="">注册</a></li>
+        <li><a href="">我的订单</a></li>
+        <li><a href="">客户服务</a></li>
+        <li><a href="">网站导航</a></li>
+        <li><a href="">手机版</a></li>
+        <li>
+          <table></table>
+          <a href="">
+            <img src="../../public/header/gwc.png">
+            购物车
+            </a>
+          </li>
+      </ul>
+    </div>
     </div>  
   </div>  
 </template>
@@ -45,62 +45,75 @@ export default {
   margin: 0;
   padding: 0;
 }
-.el-breadcrumb__item a{
-  color:#ACACAC;
-  font-weight: 400;
-}
-div.hw-header a:hover{
-  color: #CB242B;                                                     /*所有a:hover的颜色*/
-}
-div.el-breadcrumb>.el-breadcrumb__item:nth-child(8) /deep/ .el-breadcrumb__separator{
-  display: none;
-  margin: 0;
-}
-div.el-breadcrumb>.el-breadcrumb__item:nth-child(8){
-  background: url(../img/down.jpg) no-repeat;
-  background-position: 2.8rem 1.2rem;
-  padding-right: 7rem;
-}
-div.el-breadcrumb>.el-breadcrumb__item:nth-child(8):hover{              /*头部第八个元素鼠标停留底色变白色*/
-  /* background: #fff; */
-  background: url(../img/up.png) no-repeat;
-  background-position: 2.8rem 1.2rem;
-  padding-right: 7rem;
-}
-.el-breadcrumb__item{
-    line-height: 3rem;
-    font-size: 12px;
-}
-div.hw-header {
-  display: flex;
-  justify-content: space-around;
-  background: #f9f9f9;
-  
-}
-div.hw-header .el-breadcrumb:last-child:hover>div.gdjc{                             /*鼠标停留事件*/
-  background: #fff;
-  display: block;
+
+ul{ 
+  list-style:none;
 }
 
-div.gdjc{
-  width: 7rem;
-  height: 8rem;
-  border-radius: 10px;
-  background-color: #fff;
-  display: none;
-  position: absolute;
-  top:3rem;
-  left: 37.7rem;
-  z-index: 100;
-  box-shadow: 0px 10px 10px 1px rgb(165, 164, 164);
+ul li{ float:left}
+
+.hw-header>.layout{
+  height: 1.8rem;
 }
-div.gdjc ul{
-  list-style: none;
+
+.hw-header>.layout>.s-sub::after{                      /*清除li的浮动*/
+  clear:both;
+  display: block;
+  content: "";
 }
-div.gdjc ul li{
-  float: none;
+
+.hw-header>.layout{                                   /*顶部导航栏的背景颜色宽度*/
+  width: 100%;
+  background-color:#F9F9F9;
 }
-div.gdjc ul li a{
+
+.hw-header>.layout>.s-sub,
+.hw-header>.layout>.s-main{                            /*左侧导航栏距左边的margin,左右两个div变为行内块元素*/
+  margin-left: 10rem;
+  display: inline-block;
+}
+
+.hw-header>.layout>.s-sub>ul>li::after,
+.hw-header>.layout>.s-main>ul>li::after{               /*每个li后面加上 | 和 padding*/
+  content: "|";
+  color:#D3D3D3;
+  position: relative;
+  left:9px; 
+  top: 0px;
+}
+.hw-header>.layout>.s-sub>ul>li:last-child::after{        /*左边列表 最后一个li的content内容清空*/
+  content: "";
+}
+.hw-header>.layout>.s-sub>ul>li:last-child a::after{      /*左边列表 最后一个li的content加上∨*/
+  content: "∨";
+}
+.hw-header>.layout>.s-main>ul>li:nth-child(1)::after,
+.hw-header>.layout>.s-main>ul>li:last-child::after,
+.hw-header>.layout>.s-main>ul>li:nth-child(6)::after{     /*右边列表 第一个和最后一个li清除content*/
+  content: "";
+}
+.hw-header>.layout>.s-main>ul>li:nth-child(4) a::after,   /*第四个第五个第六个li更换content*/
+.hw-header>.layout>.s-main>ul>li:nth-child(5) a::after,
+.hw-header>.layout>.s-main>ul>li:nth-child(6) a::after{
+  content: "∨";
+}
+.hw-header>.layout>.s-main>ul>li:nth-child(7) img{
+  vertical-align: middle;
+}
+.hw-header>.layout>.s-main>ul>li:last-child{
+  background-color: #F2F2F2;
+}
+
+.hw-header>.layout>.s-sub a,
+.hw-header>.layout>.s-main a{
   font-size: 12px;
+  color: #A4A4A4;
 }
+
+.hw-header>.layout>.s-sub li,
+.hw-header>.layout>.s-main li{                          /*每个li左右的padding距离*/                  
+  padding: 0 8px;
+}
+
+
 </style>
