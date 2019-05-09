f960859e69dc4faa78b6f7d7c6b2081ab157c61d
BianXuerui <50104688+BianXuerui@users.noreply.github.com>
2019-05-09 20:12:30 +0800

    index.vue
    注意引入文件的路径

---
+++ product_vue_huawei/src/views/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="header">
+            <my-header></my-header>
+        </div>
+        <div class="rotation">
+            <my-rotation></my-rotation>
+        </div>
+        <div class="rotation1">
+            <my-rotation1></my-rotation1>
+        </div>
+        
+    </div>
+</template>
+<script>
+import myHeader from '@/components/Header.vue'
+import myRotation from '@/views/Rotation.vue'
+import myRotation1 from '@/views/Rotation1.vue'
+export default {
+    components:{
+        myHeader,
+        myRotation,
+        myRotation1,
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style scope>
+
+</style>
