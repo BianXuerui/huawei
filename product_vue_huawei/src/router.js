7b67e31126e608ffb42a092800eba88a30dad477
BianXuerui <50104688+BianXuerui@users.noreply.github.com>
2019-05-11 17:16:12 +0800

    router.js
    添加了一楼 二楼 三楼的三个路由地址

--- product_vue_huawei/src/router.js
+++ product_vue_huawei/src/router.js
@@ -4,11 +4,17 @@ import index from './views/index.vue'
 import Header from './components/Header.vue'
 import Rotation from './components/header/Rotation.vue'
 import Rotation1 from './components/header/Rotation1.vue'
+import firstFloor from './components/container/firstFloor.vue'
+import secondFloor from './components/container/secondFloor.vue'
+import thirdFloor from './components/container/thirdFloor.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:'/thirdFloor',component:thirdFloor},
+    {path:'/secondFloor',component:secondFloor},
+    {path:'/firstFloor',component:firstFloor},
     {path:'/Rotation1',component:Rotation1},
     {path:'/Rotation',component:Rotation},
     {path:'/Header',component:Header},
