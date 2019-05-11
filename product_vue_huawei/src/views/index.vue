dc5484c2d891758afaa204338e48e5215efb0f80
BianXuerui <50104688+BianXuerui@users.noreply.github.com>
2019-05-11 17:11:27 +0800

    index.vue
    添加了一二三楼的子页面

--- product_vue_huawei/src/views/index.vue
+++ product_vue_huawei/src/views/index.vue
@@ -9,18 +9,32 @@
         <div class="rotation1">
             <my-rotation1></my-rotation1>
         </div>
-        
+        <div class="firstFloor">
+            <my-firstFloor></my-firstFloor>
+        </div>
+        <div class="secondFloor">
+            <my-secondFloor></my-secondFloor>
+        </div>
+        <div class="thirdFloor">
+            <my-thirdFloor></my-thirdFloor>
+        </div>
     </div>
 </template>
 <script>
 import myHeader from '@/components/Header.vue'
-import myRotation from '@/components/Rotation.vue'
-import myRotation1 from '@/components/Rotation1.vue'
+import myRotation from '@/components/header/Rotation.vue'
+import myRotation1 from '@/components/header/Rotation1.vue'
+import myFirstFloor from '@/components/container/firstFloor.vue'
+import mySecondFloor from '@/components/container/secondFloor.vue'
+import myThirdFloor from '@/components/container/thirdFloor.vue'
 export default {
     components:{
         myHeader,
         myRotation,
         myRotation1,
+        myFirstFloor,
+        mySecondFloor,
+        myThirdFloor,
     },
     data() {
         return {
