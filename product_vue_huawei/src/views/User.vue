73d045ec4d59d361087d6a7f8585439ce062ff76
BianXuerui <50104688+BianXuerui@users.noreply.github.com>
2019-05-29 20:19:26 +0800

    User.vue
    用户页的整体视图

---
+++ product_vue_huawei/src/views/User.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div>
+            <user-header></user-header>
+        </div>
+        <div>
+            <user-container></user-container>
+        </div>
+        <div>
+            <user-footer></user-footer>
+        </div>
+    </div>
+</template>
+<script>
+import userHeader from '@/components/user/userHeader.vue'
+import userContainer from '@/components/user/container.vue'
+import userFooter from '@/components/user/userFooter.vue'
+
+export default {
+    data(){
+        return {}
+    },
+    components:{
+        userHeader,
+        userContainer,
+        userFooter,
+    }   
+}
+</script>
+<style>
+body{
+    overflow-x: hidden;
+}
+</style>
+
