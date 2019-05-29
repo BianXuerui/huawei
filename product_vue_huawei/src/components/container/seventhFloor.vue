8a24cd6b74bed81dc42100a633eaad3b3ea741d9
BianXuerui <50104688+BianXuerui@users.noreply.github.com>
2019-05-29 20:06:55 +0800

    seventhFloor.vue
    完成左右移动商品功能

--- product_vue_huawei/src/components/container/seventhFloor.vue
+++ product_vue_huawei/src/components/container/seventhFloor.vue
@@ -14,60 +14,120 @@
         <div class="second">
             <ul>
                 <li><a href=""><img src="../../../public/container/seventhFloor/11.jpg"></a></li>
-                <li>
+                <li v-for="item in watchesList" :key="item.id">
                     <a href="">
-                        <div class="gridImg"><img src="../../../public/container/seventhFloor/12.png"></div>
+                        <div class="gridImg"><img :src="'http://127.0.0.1:3000/'+item.pimg"></div>
                     </a>
-                    <div class="gridInfo">荣耀手表魔法系列</div>
-                    <p class="gridDescribe">轻薄设计 强劲续航</p>
-                    <p class="gridPrice">￥899</p>
+                    <div class="gridInfo">{{item.ptitle}}</div>
+                    <p class="gridDescribe">{{item.pdesc}}</p>
+                    <p class="gridPrice">￥{{item.pprice}}</p>
+                    <p class="gridTips" v-if="item.ptips.length>0">
+                            <em>
+                                <span>{{item.ptips}}</span>
+                            </em>
+                        </p>
                 </li>
+                <!-- <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
+                <li><a href=""></a></li> -->
             </ul>
         </div>
         <!-- 三楼 -->
         <div class="third">
-            <ul>
-                <li>
-                    <a href="">
+            <ul :style="ulImgStyle">
+                <li v-for="item in myList" :key="item.id">
+                    <a href="javascript:;">
                         <div class="thirdImg">
                             <p class="thirdImgP">
-                            <img src="../../../public/container/seventhFloor/31.png">
+                            <img :src="'http://127.0.0.1:3000/'+item.pimg">
                             </p>
                             <p class="gridDescribe">
-                                七重定位精准守候
+                                {{item.pdesc}}
                             </p>
                         </div>
                         <div class="gridTitle">
-                            荣耀小K 2 儿童手表
+                            {{item.ptitle}}
                         </div>
                         <p class="gridPrice">
-                            ￥299
+                            ￥{{item.pprice}}
+                        </p>
+                        <p class="gridTips" v-if="item.ptips.length>0">
+                            <em>
+                                <span>{{item.ptips}}</span>
+                            </em>
                         </p>
                     </a>
                 </li>
+                <!-- <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
+                <li><a href=""></a></li> -->
             </ul>
+            <div class="btnLeft" :class="btnLeftDisabled==true?'hidden':''" @click="movedRight">
+                <img src="../../../public/container/thirdFloor/left.png">
+            </div>
+            <div class="btnRight" :class="btnRightDisabled==true?'hidden':''" @click="movedLeft">
+                <img src="../../../public/container/thirdFloor/right.png">
+            </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+    props:["watchesList"],
+    data(){
+        return {
+            ulImgStyle:{
+                width:0,
+                'margin-left':0, 
+            },
+            moved:0,
+            myList:{}
+        }
+    },
+    computed:{
+        btnLeftDisabled(){
+            return this.moved == 0;
+        },
+        btnRightDisabled(){
+            return this.moved >= this.myList.length-6
+        }
+    },
+    methods:{
+        movedLeft(){
+            if(this.btnRightDisabled==false){
+                this.moved++;
+                this.ulImgStyle['margin-left'] = this.moved*-200+"px";
+            }
+        },
+        movedRight(){
+            if(this.btnLeftDisabled == false){
+                this.moved--;
+                this.ulImgStyle['margin-left'] = this.moved*-200+"px";
+            }
+        },
+        LoadList(){
+        var url = "http://127.0.0.1:3000/listTwo";
+        this.axios.get(url).then((result)=>{
+            this.myList = result.data.result;
+            this.ulImgStyle.width = this.myList.length*200+"px"
+        })
+      }
+    },
+    created(){
+        this.LoadList();
+    }
 }
+
 </script>
 <style>
 *{margin: 0;padding: 0;}
+em{font-style: normal;font-weight: 400;width: 100%;height: 100%;}
 #seventhFloor{                                                              /*整个七楼容器的大小和位置*/
     width: 1200px;
     margin: 2rem auto 0; 
@@ -110,6 +170,7 @@ export default {
     background-color: #F9F9F9;
     margin-left: 10px;
     border-radius: 10px;
+    position: relative;
 }
 #seventhFloor>.second>ul>li:nth-child(5),                                   /*第二个容器第二排li距离上面和下面的距离*/
 #seventhFloor>.second>ul>li:nth-child(6),
@@ -152,6 +213,8 @@ export default {
     line-height: 50px;
 }
 
+
+
 #seventhFloor>.third>ul>li:first-child{                                     /*第三个容器里面,第一个li左边外边距为0*/
     margin-left: 0px;
 }
@@ -160,7 +223,16 @@ export default {
     background-color: #F9F9F9;
     margin-left: 10px;
     border-radius: 10px;
+    position: relative;
 }
+#seventhFloor>.third{                                                       /*让列表里面多余的商品隐藏*/
+    overflow: hidden;
+    height: 15rem;
+    position: relative;
+}
+/* #seventhFloor>.third>ul>li:nth-child(7){                                    
+    margin-left: 0px;
+} */
 #seventhFloor>.third>ul>li>a>.thirdImg>.thirdImgP>img{                      /*第三个容器里,img图片的大小*/
     width: 120px;height: 120px;
     margin-top: 35px;
@@ -182,6 +254,60 @@ export default {
 #seventhFloor>.third>ul>li>a>.gridPrice{
     line-height: 10px;
     color: #D00230;
+}
+
+#seventhFloor .gridTips{
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    /* border: 1px solid; */
+    height: 48px;
+    display: block;
+}  
+#seventhFloor .gridTips span{   
+    display: inline-block;                                      /*爆款框*/
+    padding: 0 9px;
+    margin: 0 auto;
+    height: 22px;
+    /* line-height: 22px; */
+    color: #fff;
+    border-radius: 0 0 6px 6px;
+    background-color: #ff8486;
+}
+
+#seventhFloor .hidden{
+    display: none;
+}
+
+#seventhFloor .btnLeft,                                         /*左右两个按钮容器框的大小*/
+#seventhFloor .btnRight{
+    width: 36px;height: 73px;
+    background-color: #F0F0F0;  
+}
+#seventhFloor .btnLeft{                                         /*左边按钮框的位置样式*/
+    position: absolute;
+    top: 65px;
+    left: 0px;
+    border-radius: 0 10px 10px 0;
+}
+#seventhFloor .btnRight{                                        /*右边按钮框的位置样式*/
+    position: absolute;
+    top: 65px;
+    left: 1154px;
+    border-radius: 10px 0 0 10px;
+}
+#seventhFloor .btnLeft img{                                     /*左边按钮框的图片的大小位置*/
+    width: 35px;height: 35px;
+    position: absolute;
+    top: 19px;
+    left: 0px;
+}
+#seventhFloor .btnRight img{                                    /*右边按钮框的图片的大小位置*/
+    width: 35px;height: 35px;
+    position: absolute;
+    top: 19px;
+    left: 0px;
 }
 </style>
 
