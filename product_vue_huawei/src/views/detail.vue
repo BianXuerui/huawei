88709663f99f8073801a5d8eb7e77c7501bb36c1
BianXuerui <50104688+BianXuerui@users.noreply.github.com>
2019-06-05 08:13:56 +0800

    detail.vue
    商品详情页

---
+++ product_vue_huawei/src/views/detail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>
+            <myHeader></myHeader>
+        </div>
+    </div>
+</template>
+<script>
+import myHeader from '@/components/header'
+export default {
+    data(){
+        return {}
+    },
+    components:{
+        myHeader
+    },
+}
+</script>
+<style>
+
+</style>
+
